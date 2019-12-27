1e486a9f476fb601fca655ff07a77485625a282b
Iliyas Attarwala <iliyasattarwala007@gmail.com>
2019-12-27 18:04:26 +0530

    p2psync in updation and minor updations

--- app/index.js
+++ app/index.js
@@ -16,7 +16,7 @@ const p2pServer = new P2pServer(bc);
 //body parser is used to recieve data from post request in a specific format
 app.use(bodyParser.json());
 
-//removeBackupFile();
+//ChainUtil.removeBackupFile();
 
 if (!fs.existsSync(BACKUP_DIR)){
 	console.log("Creating Backup folder... created!");
@@ -36,11 +36,6 @@ try {
 	console.error(err);
 }
 
-async function removeBackupFile() {
-	await fs.unlinkSync(BACKUP_PATH);
-}
-
-
 //it shows all the blocks in the blockchain
 app.get('/api/blocks', (req, res)=> {
     res.json(bc.chain);
@@ -57,7 +52,11 @@ app.post('/api/mine', (req, res) => {
 });
 
 app.post('/api/update', (req,res) => {
-    res.json({ message: bc.update(req.body.publicKey, req.body.update) });
+	const block = bc.update(req.body.publicKey, req.body.update);
+
+	p2pServer.syncChains();
+    
+    res.json({ message: block });
 });
 
 app.post('/api/verify', (req,res) => {
